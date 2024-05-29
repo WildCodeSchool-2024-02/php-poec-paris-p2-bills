55eebe9a965364d4683040d32f28796720a45b88
Youssef750XX <bendjebbouryoussef@hotmail.fr>
2024-05-29 16:33:07 +0200

    Initialisation Terminée

--- database.sql
+++ database.sql
@@ -1,63 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
--- http://www.phpmyadmin.net
---
--- Client :  localhost
--- Généré le :  Jeu 26 Octobre 2017 à 13:53
--- Version du serveur :  5.7.19-0ubuntu0.16.04.1
--- Version de PHP :  7.0.22-0ubuntu0.16.04.1
+-- Création de la table USER
+CREATE TABLE `user` (
+    `id_user` INT NOT NULL AUTO_INCREMENT,
+    `user_firstname` VARCHAR(255) NOT NULL,
+    `user_lastname` VARCHAR(255) NOT NULL,
+    `user_siret` CHAR(14) NOT NULL,
+    `user_address` VARCHAR(255) NOT NULL,
+    `user_email` VARCHAR(255) NOT NULL,
+    `user_password` VARCHAR(255) NOT NULL,
+    `user_bank_details` VARCHAR(255),
+    PRIMARY KEY (`id_user`)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- Création de la table Invoices
+CREATE TABLE `invoice` (
+    `id_invoice` INT NOT NULL AUTO_INCREMENT,
+    `id_user` INT NOT NULL,
+    `created_at` DATE NOT NULL,
+    `due_date` DATE NOT NULL,
+    `total_amount` DECIMAL(10,2) NOT NULL,
+    `bill_status` VARCHAR(255),
+    `client_siret` CHAR(14),
+    `client_name` VARCHAR(255),
+    `client_address` VARCHAR(255),
+    PRIMARY KEY (`id_invoice`),
+    FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`)
+);
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données :  `simple-mvc`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `item`
---
-
-CREATE TABLE `item` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Contenu de la table `item`
---
-
-INSERT INTO `item` (`id`, `title`) VALUES
-(1, 'Stuff'),
-(2, 'Doodads');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `item`
---
-ALTER TABLE `item`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `item`
---
-ALTER TABLE `item`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Création de la table Product
+CREATE TABLE `product` (
+    `id_product` INT NOT NULL AUTO_INCREMENT,
+    `id_invoice` INT NOT NULL,
+    `product_name` VARCHAR(255) NOT NULL,
+    `product_quantity` INT NOT NULL,
+    `product_price` DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (`id_product`),
+    FOREIGN KEY (`id_invoice`) REFERENCES `invoice`(`id_invoice`)
+);
