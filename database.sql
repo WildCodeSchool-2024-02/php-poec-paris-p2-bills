6bdd9ff59414ef6583a0c9c3bfd6231aeeb9059c
Youssef750XX <bendjebbouryoussef@hotmail.fr>
2024-05-30 14:39:32 +0200

    Fix database.sql

--- database.sql
+++ database.sql
@@ -1,38 +1,38 @@
 -- Création de la table USER
 CREATE TABLE `user` (
-    `id_user` INT NOT NULL AUTO_INCREMENT,
-    `user_firstname` VARCHAR(255) NOT NULL,
-    `user_lastname` VARCHAR(255) NOT NULL,
-    `user_siret` CHAR(14) NOT NULL,
-    `user_address` VARCHAR(255) NOT NULL,
-    `user_email` VARCHAR(255) NOT NULL,
-    `user_password` VARCHAR(255) NOT NULL,
-    `user_bank_details` VARCHAR(255),
-    PRIMARY KEY (`id_user`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `firstname` VARCHAR(255) NOT NULL,
+    `lastname` VARCHAR(255) NOT NULL,
+    `siret` CHAR(14) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `bank_details` VARCHAR(255),
+    PRIMARY KEY (`id`)
 );
 
 -- Création de la table Invoices
 CREATE TABLE `invoice` (
-    `id_invoice` INT NOT NULL AUTO_INCREMENT,
-    `id_user` INT NOT NULL,
-    `created_at` DATE NOT NULL,
-    `due_date` DATE NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `total_amount` DECIMAL(10,2) NOT NULL,
     `bill_status` VARCHAR(255),
+    `due_at` DATE NOT NULL,
     `client_siret` CHAR(14),
     `client_name` VARCHAR(255),
     `client_address` VARCHAR(255),
-    PRIMARY KEY (`id_invoice`),
-    FOREIGN KEY (`id_user`) REFERENCES `user`(`id_user`)
+    `created_at` DATE NOT NULL,
+    `user_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 );
 
 -- Création de la table Product
 CREATE TABLE `product` (
-    `id_product` INT NOT NULL AUTO_INCREMENT,
-    `id_invoice` INT NOT NULL,
-    `product_name` VARCHAR(255) NOT NULL,
-    `product_quantity` INT NOT NULL,
-    `product_price` DECIMAL(10,2) NOT NULL,
-    PRIMARY KEY (`id_product`),
-    FOREIGN KEY (`id_invoice`) REFERENCES `invoice`(`id_invoice`)
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `quantity` INT NOT NULL,
+    `price` DECIMAL(10,2) NOT NULL,
+    `invoice_id` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`invoice_id`) REFERENCES `invoice`(`id`)
 );
