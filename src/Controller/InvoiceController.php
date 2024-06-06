0b726c7fd0dbf1c8a772de75cbc99826fc713f12
Youssef750XX <bendjebbouryoussef@hotmail.fr>
2024-06-06 19:56:15 +0200

    Fonctionalités de création de facture

---
+++ src/Controller/InvoiceController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Controller;
+
+use App\Model\InvoiceManager;
+
+class InvoiceController extends AbstractController
+{
+    private InvoiceManager $invoiceManager;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->invoiceManager = new InvoiceManager();
+    }
+
+    /**
+     * Display create invoice page
+     */
+    public function create(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $invoice = array_map('htmlentities', array_map('trim', $_POST));
+
+            $errors = [];
+
+            if (empty($invoice['created_at']) ||
+                empty($invoice['due_at']) ||
+                empty($invoice['user_siret']) ||
+                empty($invoice['user_name']) ||
+                empty($invoice['user_address']) ||
+                empty($invoice['total_amount']))
+            {
+                $errors[] = 'Les champs marqués d\'un * sont obligatoires.';
+            }
+
+            // Si pas d'erreurs, insérer la facture puis redirection vers le dashboard
+            if (empty($errors)) {
+                $this->invoiceManager->insert($invoice);
+                header('Location: /dashboard');
+                exit;
+            }
+
+            // Affichage des erreurs dans le template
+            return $this->twig->render('Invoice/create_invoice.html.twig', [
+                'errors' => $errors,
+            ]);
+        }
+
+        // Affichage initial du formulaire
+        return $this->twig->render('Invoice/create_invoice.html.twig');
+    }
+}
---
+++ src/Model/InvoiceManager.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Model;
+
+use PDO;
+
+class InvoiceManager extends AbstractManager
+{
+    public const TABLE2 = 'invoice';
+    public const TABLE3 = 'product';
+
+    /**
+     * Insert invoice in database
+     */
+    public function insert(array $invoice): void
+    {
+        // Insérer les données dans la table 'invoice'
+        $invoiceQuery = "INSERT INTO " . self::TABLE2 . "
+                 (total_amount, created_at, due_at, user_siret, user_name, user_address, user_bank_details,
+                 client_siret, client_name, client_address, user_id)
+                 VALUES
+                 (:total_amount, :created_at, :due_at, :user_siret, :user_name, :user_address, :user_bank_details,
+                 :client_siret, :client_name, :client_address, :user_id)";
+
+        $invoiceStatement = $this->pdo->prepare($invoiceQuery);
+
+        $invoiceStatement->bindValue(':total_amount', $invoice['total_amount'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':created_at', $invoice['created_at'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':due_at', $invoice['due_at'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':user_siret', $invoice['user_siret'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':user_name', $invoice['user_name'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':user_address', $invoice['user_address'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':user_bank_details', $invoice['user_bank_details'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':client_siret', $invoice['client_siret'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':client_name', $invoice['client_name'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':client_address', $invoice['client_address'], PDO::PARAM_STR);
+        $invoiceStatement->bindValue(':user_id', 1, PDO::PARAM_INT); // CODER EN DURE POUR LE MOMENT CHANGER PLUS TARD
+
+        $invoiceStatement->execute();
+
+        // Récupérer l'ID de la facture nouvellement insérée
+        $invoiceId = $this->pdo->lastInsertId();
+
+        $products = [];
+
+        // On boucle sur le tableau invoice
+        foreach($invoice as $key => $value) {
+            // On verifie les clés commencant par "product_name_"
+            if (strpos($key, 'product_name_') === 0){
+
+                // Extraire le numéro du produit
+                $product_number = substr($key, strlen('product_name_'));
+
+                // Récupérer les valeurs des autres attributs du produit
+                $name = $invoice['product_name_' . $product_number];
+                $price = $invoice['product_price_' . $product_number];
+                $quantity = $invoice['product_quantity_' . $product_number];
+
+                // Ajouter les données du produit au tableau
+                $products[] = array(
+                    'name' => $name,
+                    'price' => $price,
+                    'quantity' => $quantity,
+                );
+            }
+        }
+
+        // Préparer la requête d'insertion
+        $productQuery = "INSERT INTO " . self::TABLE3 . " (name, quantity, price, invoice_id) VALUES (:name, :quantity, :price, :invoice_id)";
+
+        $productStatement = $this->pdo->prepare($productQuery);
+
+        // Insérer les données des produits dans la base de données
+        foreach ($products as $product)
+        {
+            $productStatement->bindParam(':name', $product['name']);
+            $productStatement->bindParam(':quantity', $product['quantity']);
+            $productStatement->bindParam(':price', $product['price']);
+            $productStatement->bindParam(':invoice_id', $invoiceId);
+            $productStatement->execute();
+        }
+    }
+}
